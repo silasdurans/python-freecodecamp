5d801cd55991df33cb24280c019ad59d055811ef
silasdurans <silasdurans@hotmail.com>
2026-08-16 19:49:58 -0300

    finaliza atividade

--- 01-python-basics/build-a-bill-spliter.py
+++ 01-python-basics/build-a-bill-spliter.py
@@ -18,3 +18,6 @@ print('Total with tip:', running_total)
 
 final_bill = running_total / num_of_friends
 print('Bill per person:', final_bill)
+
+each_pays = round(final_bill,2)
+print('Each person pays:', each_pays)
